ae96058a0674defadf5537f842c3dbf83070ae39
nsevo <pocosy@outlook.com>
2025-12-02 17:17:33 +0800

    feat: 更新Options的UnmarshalJSON方法，支持hysteria2核心类型并优化核心选择逻辑

--- conf/node.go
+++ conf/node.go
@@ -123,13 +123,17 @@ func (o *Options) UnmarshalJSON(data []byte) error {
 	if err != nil {
 		return err
 	}
-	// Only xray core is supported, set default if empty
-	if o.Core == "" {
-		o.Core = "xray"
+	// Core can be empty - Selector will auto-select based on node type
+	// Supported cores: xray, hysteria2
+	switch o.Core {
+	case "", "xray":
+		// Default to xray, parse XrayOptions
+		o.XrayOptions = NewXrayOptions()
+		return json.Unmarshal(data, o.XrayOptions)
+	case "hysteria2", "hy2":
+		// Hysteria2 doesn't need XrayOptions
+		return nil
+	default:
+		return fmt.Errorf("unsupported core type: %s (supported: xray, hysteria2)", o.Core)
 	}
-	if o.Core != "xray" {
-		return fmt.Errorf("unsupported core type: %s (only 'xray' is supported)", o.Core)
-	}
-	o.XrayOptions = NewXrayOptions()
-	return json.Unmarshal(data, o.XrayOptions)
 }
